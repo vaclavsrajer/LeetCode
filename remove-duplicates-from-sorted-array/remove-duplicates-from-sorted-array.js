ad2db241fc303da33ea1c6a41fecba1ba53294d9
vaclavsrajer <79576005+vaclavsrajer@users.noreply.github.com>
2022-07-07 18:09:05 +0200

    Time: 143 ms, Memory: 44.3 MB - LeetHub

--- remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -3,10 +3,16 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    for(let i = 1; i <= nums.length; i++){
-        if(nums[i-1] === nums[i]){
-            nums.splice(i,1);
-            i--;
+    if(nums.length === 1){
+        return nums;
+    }
+    let index = 1;
+    for(let i = 1; i < nums.length; i++){
+        if(nums[i-1] !== nums[i]){
+            nums[index] = nums[i];
+            index++;
         }
     }
+    
+    return index;
 };
