f4f4c91e2f2530ada04a5b8be5088072c1b98adc
vaclavsrajer <79576005+vaclavsrajer@users.noreply.github.com>
2022-07-07 17:06:50 +0200

    Time: 89 ms, Memory: 42.1 MB - LeetHub

---
+++ merge-sorted-array/merge-sorted-array.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    if(n === 0){
+        return nums1;
+    }
+    
+    for(let i = m; i < nums1.length; i++){
+        nums1[i] = nums2[i-m];
+    }
+    
+    nums1.sort((a,b) => a-b);
+    return nums1;
+};
