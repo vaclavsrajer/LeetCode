f97a17d58ed261b9d3476e672709823eb794f446
vaclavsrajer <79576005+vaclavsrajer@users.noreply.github.com>
2022-07-08 12:49:01 +0200

    Time: 187 ms, Memory: 48.9 MB - LeetHub

--- squares-of-a-sorted-array/squares-of-a-sorted-array.js
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.js
@@ -3,20 +3,21 @@
  * @return {number[]}
  */
 var sortedSquares = function(nums) {
-    let result = [];
-    let left = 0;
-    let right = nums.length - 1;
+        let n = nums.length;
+        let result = [];
+        let left = 0;
+        let right = n - 1;
     
-    for(let i = nums.length -1; i >= 0; i--){
+    for(let i = n-1; i >=0; i--){
         let square;
         if(Math.abs(nums[left]) < Math.abs(nums[right])){
             square = nums[right];
             right--;
-        }else {
+        }else{
             square = nums[left];
             left++;
         }
-        result[i] = square* square;
+        result[i] = square * square;
     }
     return result;
 };
